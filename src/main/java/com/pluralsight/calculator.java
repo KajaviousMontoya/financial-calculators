96ffbe0e213c7055dea4f5c93c22ca85b0117f63
kajaviousmontoya <montoyakajavious@gmail.com>
2026-04-10 10:44:42 -0700

    i updated the doubles (interest rates and the loan payment in general) updated my int and used math.pow, finally figured out how much the monthly payment would be

--- src/main/java/com/pluralsight/calculator.java
+++ src/main/java/com/pluralsight/calculator.java
@@ -3,10 +3,13 @@ package com.pluralsight;
 public class calculator {
     public static void main(String[] args) {
         double P = 43000;
-        double yearlyRate = 0.053;
+        double yearlyRate = 0.053 / 12;
         double i = yearlyRate / 12; // monthly interest rate
         int n = 10 * 12; // total months multiply by years
         double numerator = i * Math.pow(1 + i, n);
         double denominator = Math.pow(1 + i, n) - 1;
+        double M = P * (numerator / denominator);
+        System.out.printf("Monthly Payment: %.2f\n", M); // start of format, the decimal places, and f for float
+
     }
 }
